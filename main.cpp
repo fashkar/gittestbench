484ae3dcd67fe68c0fd319015a1996345756d423
ferar ashkar <fashkar@mm-sol.com>
2015-11-25 10:02:05 +0200

    added for loop

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 int main(int argc, char **argv) {
     std::cout << "Hello, world!" << std::endl;
 
+	for(int i=0;i<10;++i)
+		std::cout << i << std::endl;
 
 	std::cout << "bye, world!" << std::endl;
     return 0;
